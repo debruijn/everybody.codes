207a1d934b5172704289b17ad814bf48eeafdab7
Bert de Bruijn <lpdebruijn@gmail.com>
2024-11-11 19:00:25 -0400

    Update main.rs to read the plugged in file

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,3 +1,13 @@
+use itertools::Itertools;
+
+mod util;
+
+fn run<'a>(input_str: Vec<String>) -> &'a str {
+    let res = input_str;
+    res.iter().concat()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_str = util::read_input(YEAR, DAY);
+    println!("{}", run(input_str))
 }
