0de643828b17df17fac399bfb89c2a1c2dea3b39
Bert de Bruijn <lpdebruijn@gmail.com>
2024-11-12 15:46:21 -0400

    Changes due to no year/day input needed in read_input in specific folder

--- Makefile
+++ Makefile
@@ -14,9 +14,7 @@ create: create_day_folder
 	cp template/Cargo.toml ${YEAR}/${DAY}/Cargo.toml
 	cp template/src/util.rs ${YEAR}/${DAY}/src/util.rs
 	cp template/src/main.rs ${YEAR}/${DAY}/src/main.rs
-	sed -i 's/_dayxx/_day_${DAY}/g' ${YEAR}/${DAY}/Cargo.toml
-	sed -i 's/DAY/${DAY}/g' ${YEAR}/${DAY}/src/main.rs
-	sed -i 's/YEAR/${YEAR}/g' ${YEAR}/${DAY}/src/main.rs
+	sed -i 's/_dayxx/_${YEAR}_${DAY}/g' ${YEAR}/${DAY}/Cargo.toml
 
 add_empty_example_file:
 	for i in $$(seq 1 ${N_EXAMPLES}); do \
--- template/src/util.rs
+++ template/src/util.rs
@@ -1,16 +1,14 @@
 use itertools::Itertools;
 use std::fs;
 
-pub fn read_input<'a>(year: isize, day: isize, num: isize) -> Vec<String> {
+pub fn read_input<'a>(mut num: isize) -> Vec<String> {
     let input_type = if num > 0 {
         "data"
     } else {
-        let num = -num;
+        num = -num;
         "example"
     };
-    let file_path = String::from(
-        (year.to_string()) + "/" + &*day.to_string() + "/" + input_type + &*num.to_string() + ".txt",
-    );
+    let file_path = String::from(input_type.to_owned() + &*num.to_string() + ".txt");
     let contents = fs::read_to_string(&file_path)
         .expect(&format!("File {} does not exist but it should!", file_path))
         .trim()
