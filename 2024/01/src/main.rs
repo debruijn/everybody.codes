db92d306074577098668d69f0e98dc7ab09ce414
Bert de Bruijn <lpdebruijn@gmail.com>
2024-11-12 15:43:40 -0400

    Do some formatting

--- 2024/01/src/main.rs
+++ 2024/01/src/main.rs
@@ -24,7 +24,6 @@ fn run_part2<'a>(input_str: Vec<String>) -> String {
         }
     }
     for battle in &input_str[0].chars().chunks(2) {
-
         for creature in battle {
             let increase = match creature {
                 'A' => 0,
@@ -42,16 +41,15 @@ fn run_part2<'a>(input_str: Vec<String>) -> String {
 fn run_part3<'a>(input_str: Vec<String>) -> String {
     let mut nr_potions = 0;
     for battle in &input_str[0].chars().chunks(3) {
-        let sum_x = battle.map(|x| if x == 'x' {0} else {1}).sum();
+        let sum_x = battle.map(|x| if x == 'x' { 0 } else { 1 }).sum();
         let increase = match sum_x {
             2 => 2,
             3 => 6,
-            _ => 0
+            _ => 0,
         };
         nr_potions += increase;
     }
     for battle in &input_str[0].chars().chunks(3) {
-
         for creature in battle {
             let increase = match creature {
                 'A' => 0,
@@ -69,16 +67,15 @@ fn run_part3<'a>(input_str: Vec<String>) -> String {
 fn run_all<'a>(input_str: Vec<String>, part: usize) -> String {
     let mut nr_potions = 0;
     for battle in &input_str[0].chars().chunks(part) {
-        let sum_x = battle.map(|x| if x == 'x' {0} else {1}).sum();
+        let sum_x = battle.map(|x| if x == 'x' { 0 } else { 1 }).sum();
         let increase = match sum_x {
             2 => 2,
             3 => 6,
-            _ => 0
+            _ => 0,
         };
         nr_potions += increase;
     }
     for battle in &input_str[0].chars().chunks(part) {
-
         for creature in battle {
             let increase = match creature {
                 'A' => 0,
@@ -94,19 +91,18 @@ fn run_all<'a>(input_str: Vec<String>, part: usize) -> String {
 }
 
 fn main() {
-
     // Initial solutions: dedicated function per part
 
     // Part 1: example and actual
     println!("Part 1");
     let input_str = util::read_input(-1);
     println!("Example: {}", run_part1(input_str));
-    let input_str = util::read_input( 1);
+    let input_str = util::read_input(1);
     println!("Actual: {}\n", run_part1(input_str));
 
     // Part 2: example and actual
     println!("Part 2");
-    let input_str = util::read_input( -2);
+    let input_str = util::read_input(-2);
     println!("Example: {}", run_part2(input_str));
     let input_str = util::read_input(2);
     println!("Actual: {}\n", run_part2(input_str));
@@ -115,7 +111,7 @@ fn main() {
     println!("Part 3");
     let input_str = util::read_input(-3);
     println!("Example: {}", run_part3(input_str));
-    let input_str = util::read_input( 3);
+    let input_str = util::read_input(3);
     println!("Actual: {}\n", run_part3(input_str));
 
     // Improved solution: shared functionality that encapsulates all parts
@@ -123,7 +119,7 @@ fn main() {
         println!("Part {}", i);
         let input_str = util::read_input(-i);
         println!("Example: {}", run_all(input_str, i as usize));
-        let input_str = util::read_input( i);
+        let input_str = util::read_input(i);
         println!("Actual: {}\n", run_all(input_str, i as usize));
     }
 }
