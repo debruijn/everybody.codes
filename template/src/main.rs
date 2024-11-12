b5b5c335e54eb0954225945374706148657c4010
Bert de Bruijn <lpdebruijn@gmail.com>
2024-11-12 15:50:57 -0400

    Align template main.rs with some changes in first actual challenge

--- template/src/main.rs
+++ template/src/main.rs
@@ -2,37 +2,37 @@ use itertools::Itertools;
 
 mod util;
 
-fn run_part1<'a>(input_str: Vec<String>) -> &'a str {
+fn run_part1(input_str: Vec<String>) -> String {
     let res = input_str;
-    res.iter().concat()
+    res.join("")
 }
 
-fn run_part2<'a>(input_str: Vec<String>) -> &'a str {
+fn run_part2(input_str: Vec<String>) -> String {
     let res = input_str;
-    res.iter().concat()
+    res.join("")
 }
 
-fn run_part3<'a>(input_str: Vec<String>) -> &'a str {
+fn run_part3<'a>(input_str: Vec<String>) -> String {
     let res = input_str;
-    res.iter().concat()
+    res.join("")
 }
 
 fn main() {
-    let input_str = util::read_input(YEAR, DAY, -1);
+    let input_str = util::read_input(-1);
     println!("{}", run_part1(input_str));
 
-    let input_str = util::read_input(YEAR, DAY, 1);
+    let input_str = util::read_input(1);
     println!("{}", run_part1(input_str));
 
-    let input_str = util::read_input(YEAR, DAY, -2);
+    let input_str = util::read_input(-2);
     println!("{}", run_part1(input_str));
 
-    let input_str = util::read_input(YEAR, DAY, 2);
+    let input_str = util::read_input(2);
     println!("{}", run_part2(input_str));
 
-    let input_str = util::read_input(YEAR, DAY, -3);
+    let input_str = util::read_input(-3);
     println!("{}", run_part1(input_str));
 
-    let input_str = util::read_input(YEAR, DAY, 3);
+    let input_str = util::read_input(3);
     println!("{}", run_part3(input_str));
 }
