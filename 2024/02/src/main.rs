a0d23d4e2cd18b2a7d4860d292172c3f69e03f3d
Bert de Bruijn <lpdebruijn@gmail.com>
2024-11-12 20:59:16 -0400

    Apply cargo fmt on 2024/02 - further cleanup will happen later

--- 2024/02/src/main.rs
+++ 2024/02/src/main.rs
@@ -1,16 +1,18 @@
-use std::cmp::min;
-use std::collections::HashSet;
-use itertools::Itertools;
 use everybody_codes_util as util;
+use itertools::Itertools;
+use std::collections::HashSet;
 
 fn run_part1(input_str: Vec<String>) -> String {
     let words = input_str[0][6..].split(',');
 
     let mut res = Vec::new();
     for txt in input_str[2..].iter() {
-        let this_res: usize = words.clone().into_iter().map(|x| txt.match_indices(x).collect_vec().len()).sum();
+        let this_res: usize = words
+            .clone()
+            .into_iter()
+            .map(|x| txt.match_indices(x).collect_vec().len())
+            .sum();
         res.push(this_res.to_string());
-
     }
     res.join(", ")
 }
@@ -55,11 +57,9 @@ fn run_part2(input_str: Vec<String>) -> String {
         }
 
         res.push(inds.len());
-
     }
     res.iter().sum::<usize>().to_string()
 }
-
 
 fn transpose(original: Vec<&String>) -> Vec<String> {
     assert!(!original.is_empty());
@@ -71,7 +71,10 @@ fn transpose(original: Vec<&String>) -> Vec<String> {
         }
     }
 
-    transposed.iter().map(|x|x.iter().collect::<String>()).collect_vec()
+    transposed
+        .iter()
+        .map(|x| x.iter().collect::<String>())
+        .collect_vec()
 }
 
 fn run_part3<'a>(input_str: Vec<String>) -> String {
@@ -81,7 +84,6 @@ fn run_part3<'a>(input_str: Vec<String>) -> String {
     let mut inds = HashSet::new();
 
     for (h, txt) in input_str[2..].iter().enumerate() {
-
         // TODO: cleanup below
         for word in words.clone().into_iter() {
             let this_txt = String::new() + txt + &txt[..word.len() - 1];
@@ -118,7 +120,6 @@ fn run_part3<'a>(input_str: Vec<String>) -> String {
 
     let new = transpose(input_str[2..].iter().collect_vec());
     for (v, txt) in new.iter().enumerate() {
-
         // TODO: cleanup below
         for word in words.clone().into_iter() {
             let this_txt = txt;
@@ -138,7 +139,7 @@ fn run_part3<'a>(input_str: Vec<String>) -> String {
         }
 
         for word in rev_words.clone().into_iter() {
-            let this_txt = txt;  // String::new() + txt + &txt[..min(word.len() - 1, txt.len())];
+            let this_txt = txt; // String::new() + txt + &txt[..min(word.len() - 1, txt.len())];
             let matches = this_txt.match_indices(&word).collect_vec();
             for i_match in matches.iter() {
                 for i in i_match.0..(i_match.0 + (i_match.1.len())) {
@@ -157,7 +158,6 @@ fn run_part3<'a>(input_str: Vec<String>) -> String {
         println!("{:?}", inds);
     }
     inds.len().to_string()
-
 }
 
 fn main() {
@@ -178,4 +178,10 @@ fn main() {
 
     let input_str = util::read_input(3);
     println!("{}", run_part3(input_str));
+
+    //  Cleanup part 3:
+    // - vertical removal of loop-around
+    // - repeated calls to same calc -> separate func
+    // - vertical and horizontal as well -> call same func for indices
+    // - part 2 as special case of part 3 (and perhaps part 1 as well)
 }
