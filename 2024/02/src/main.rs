f40b97fbd1874a0346bacda52c5cfe46e4e6f239
Bert de Bruijn <lpdebruijn@gmail.com>
2024-11-13 12:14:27 -0400

    Add cleaned up run_all version that can run parts 2 and 3

--- 2024/02/src/main.rs
+++ 2024/02/src/main.rs
@@ -18,6 +18,8 @@ fn run_part1(input_str: Vec<String>) -> String {
 }
 
 fn run_part2(input_str: Vec<String>) -> String {
+    // Uncleaned version -- compare with run_all below for faster cleaner version
+
     let words = input_str[0][6..].split(',');
     let rev_words = words.clone().map(|x| x.chars().rev().collect::<String>());
 
@@ -25,7 +27,6 @@ fn run_part2(input_str: Vec<String>) -> String {
     for txt in input_str[2..].iter() {
         let mut inds = HashSet::new();
 
-        // TODO: cleanup below
         for word in words.clone().into_iter() {
             let matches = txt.match_indices(word).collect_vec();
             for i_match in matches.iter() {
@@ -78,13 +79,14 @@ fn transpose(original: Vec<&String>) -> Vec<String> {
 }
 
 fn run_part3<'a>(input_str: Vec<String>) -> String {
+    // Uncleaned version -- compare with run_all below for faster cleaner version
+
     let words = input_str[0][6..].split(',');
     let rev_words = words.clone().map(|x| x.chars().rev().collect::<String>());
 
     let mut inds = HashSet::new();
 
     for (h, txt) in input_str[2..].iter().enumerate() {
-        // TODO: cleanup below
         for word in words.clone().into_iter() {
             let this_txt = String::new() + txt + &txt[..word.len() - 1];
             let matches = this_txt.match_indices(word).collect_vec();
@@ -120,7 +122,6 @@ fn run_part3<'a>(input_str: Vec<String>) -> String {
 
     let new = transpose(input_str[2..].iter().collect_vec());
     for (v, txt) in new.iter().enumerate() {
-        // TODO: cleanup below
         for word in words.clone().into_iter() {
             let this_txt = txt;
             //let this_txt = String::new() + txt + &txt[..min(word.len() - 1, txt.len())];
@@ -139,7 +140,7 @@ fn run_part3<'a>(input_str: Vec<String>) -> String {
         }
 
         for word in rev_words.clone().into_iter() {
-            let this_txt = txt; // String::new() + txt + &txt[..min(word.len() - 1, txt.len())];
+            let this_txt = txt;
             let matches = this_txt.match_indices(&word).collect_vec();
             for i_match in matches.iter() {
                 for i in i_match.0..(i_match.0 + (i_match.1.len())) {
@@ -154,9 +155,50 @@ fn run_part3<'a>(input_str: Vec<String>) -> String {
             }
         }
     }
-    if input_str.len() < 10 {
-        println!("{:?}", inds);
+
+    inds.len().to_string()
+}
+
+
+fn run_all<'a>(input_str: Vec<String>, part: isize) -> String {
+    let mut words = input_str[0][6..].split(',').map(|x| x.to_string()).collect_vec();
+    let rev_words = input_str[0][6..].split(',').map(|x| x.chars().rev().collect::<String>()).collect_vec();
+    words.extend(rev_words);
+
+    let mut inds = HashSet::new();
+
+    let input_transposed = transpose(input_str[2..].iter().collect_vec());
+
+    for (h, txt) in input_str[2..].iter().enumerate() {
+        for word in words.clone().into_iter() {
+            let this_txt = if part == 3 {
+                String::new() + txt + &txt[..word.len() - 1]
+            } else {
+                txt.to_string()
+            };
+            let mut matches = this_txt.match_indices(&word).collect_vec();
+            matches.extend(this_txt.rmatch_indices(&word).collect_vec());
+            for i_match in matches.iter() {
+                for i in i_match.0..(i_match.0 + (i_match.1.len())) {
+                    inds.insert((h, i % txt.len()));
+                }
+            }
+        }
     }
+    if part == 3 {
+        for (v, txt) in input_transposed.iter().enumerate() {
+            for word in words.clone().into_iter() {
+                let mut matches = txt.match_indices(&word).collect_vec();
+                matches.extend(txt.rmatch_indices(&word).collect_vec());
+                for i_match in matches.iter() {
+                    for i in i_match.0..(i_match.0 + (i_match.1.len())) {
+                        inds.insert((i % txt.len(), v));
+                    }
+                }
+            }
+        }
+    }
+
     inds.len().to_string()
 }
 
@@ -178,6 +220,19 @@ fn main() {
 
     let input_str = util::read_input(3);
     println!("{}", run_part3(input_str));
+
+    let input_str = util::read_input(-2);
+    println!("{}", run_all(input_str, 2));
+
+    let input_str = util::read_input(2);
+    println!("{}", run_all(input_str, 2));
+
+    let input_str = util::read_input(-3);
+    println!("{}", run_all(input_str, 3));
+
+    let input_str = util::read_input(3);
+    println!("{}", run_all(input_str, 3));
+
 
     //  Cleanup part 3:
     // - vertical removal of loop-around
