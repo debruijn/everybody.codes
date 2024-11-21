54ee146c324283cea0ebb5c264b8459fc24227b8
Bert de Bruijn <lpdebruijn@gmail.com>
2024-11-21 13:02:35 -0400

    Refactor 2024/13 to include a fast solution and an optimized slower solution

--- 2024/13/src/main.rs
+++ 2024/13/src/main.rs
@@ -3,7 +3,6 @@ use everybody_codes_util::grid::{Grid, Point};
 use itertools::Itertools;
 use std::cmp::min;
 use std::collections::HashMap;
-use std::isize::MAX;
 
 fn get_val(el: char) -> isize {
     if el == 'E' || el == 'S' || el == 'T' {
@@ -23,40 +22,40 @@ fn get_dist(x1: isize, x2: isize) -> isize {
     }
 }
 
-fn find_solution(input_str: Vec<String>, stop_at: isize, mut best_at_point: HashMap<Point<isize,2>, isize>) -> (isize, HashMap<Point<isize, 2>, isize>) {
+fn find_solution(
+    input_str: Vec<String>,
+    stop_at: isize,
+    mut best_at_point: HashMap<Point<isize, 2>, isize>,
+) -> (isize, HashMap<Point<isize, 2>, isize>) {
     let grid: Grid<char> = Grid::from_string(input_str);
+    let target = grid.filter_key('E');
     let loc = grid.filter_key('S').into_iter().next().unwrap();
-    let target = grid.filter_key('E').into_iter().next().unwrap();
 
     let mut curr_t = 0;
     let mut queue: HashMap<isize, Vec<Point<isize, 2>>> = HashMap::new();
     queue.insert(0, vec![loc]);
-    // println!("{:?}", loc);
-    // let mut best_at_point: HashMap<Point<isize, 2>, isize> = HashMap::new();
     while curr_t < stop_at {
         if !queue.contains_key(&curr_t) || queue[&curr_t].len() == 0 {
             curr_t += 1;
-            // println!("T to {curr_t}, queue keys: {:?}", queue.keys());
             continue;
         }
-        // println!("T stays at {curr_t}, queue count: {:?}", queue.iter().map(|x| x.1.len()).collect_vec());
-        // println!("{:?}", queue);
         let pt = queue.get_mut(&curr_t).unwrap().pop().unwrap();
-        if pt == target {
+        if target.contains(&pt) {
             break;
         }
 
         let this_val = get_val(grid.get_pt(pt));
         let others = grid.get_neighbors_ok(pt);
-        // println!("{:?}", others);
-        // println!("{:?}", queue);
+
         for other in others.into_iter() {
             if other.1 == '#' {
                 continue;
             }
             let o_val = get_val(other.1);
-            // println!("{:?}, {:?}, {}, {}, {}", pt, other.0, curr_t, this_val, o_val);
             let o_t = curr_t + get_dist(this_val, o_val) + 1;
+            if o_t >= stop_at {
+                continue;
+            }
             if best_at_point.keys().contains(&other.0) {
                 if best_at_point[&other.0] <= o_t {
                     continue;
@@ -70,11 +69,15 @@ fn find_solution(input_str: Vec<String>, stop_at: isize, mut best_at_point: Hash
 }
 
 fn run_part1(input_str: Vec<String>) -> String {
-    find_solution(input_str, isize::MAX, HashMap::new()).0.to_string()
+    find_solution(input_str, isize::MAX, HashMap::new())
+        .0
+        .to_string()
 }
 
 fn run_part2(input_str: Vec<String>) -> String {
-    find_solution(input_str, isize::MAX, HashMap::new()).0.to_string()
+    find_solution(input_str, isize::MAX, HashMap::new())
+        .0
+        .to_string()
 }
 
 fn replace_t_to_s(input_str: &Vec<String>, i: usize, j: usize) -> Vec<String> {
@@ -86,35 +89,57 @@ fn replace_t_to_s(input_str: &Vec<String>, i: usize, j: usize) -> Vec<String> {
 }
 
 fn run_part3(input_str: Vec<String>) -> String {
-    // Replace all S with T, temporarily
-    let adj_str = input_str.iter().map(|x| x.replace('S', "T")).collect_vec();
-
-    // Loop over all T, and replace the chosen one with S, to get score
     let dims = (input_str.len(), input_str[0].chars().count());
-    let (mut best_score, mut best_at_point) = find_solution(replace_t_to_s(&adj_str, dims.0/2, 0), isize::MAX, HashMap::new());
-    println!("{}",best_score);
-    (best_score, best_at_point) = find_solution(replace_t_to_s(&adj_str, dims.0/2, dims.1-1), best_score, best_at_point);
-    println!("{}",best_score);
-    (best_score, best_at_point)  = find_solution(replace_t_to_s(&adj_str, 0, dims.1/2), best_score, best_at_point);
-    println!("{}",best_score);
-    (best_score, best_at_point)  = find_solution(replace_t_to_s(&adj_str, dims.0-1, dims.1/2), best_score, best_at_point);
-    println!("{}",best_score);
+    let center = Point::new([dims.0 / 2, dims.1 / 2]);
 
+    // Replace all S with T, temporarily, and sort by distance to center
+    let adj_str = input_str.iter().map(|x| x.replace('S', "T")).collect_vec();
+    let mut all_ts: Vec<Point<usize, 2>> = adj_str
+        .iter()
+        .enumerate()
+        .map(|x| {
+            x.1.chars()
+                .enumerate()
+                .filter(|y| y.1 == 'T')
+                .map(move |y| Point::new([x.0, y.0]))
+        })
+        .flatten()
+        .collect_vec();
+    all_ts = all_ts
+        .into_iter()
+        .sorted_by_key(|x| x.manhattan_dist(center))
+        .collect_vec();
 
-    for (i, row) in adj_str.iter().enumerate() {
-        for (j, el) in row.chars().enumerate() {
-            if el == 'T' {
-                // println!{"{:?}", temp_str}
-                let (this_score, _best_at_point) = find_solution(replace_t_to_s(&adj_str, i, j), best_score, best_at_point);
-                best_at_point = _best_at_point;
-                if this_score < best_score {
-                    best_score = this_score;
-                    println!("{}, {}, {}", i, j, best_score)
-                }
-            }
-        }
+    // Loop over all points and update best_score and best_at_point along the way
+    let mut best_score = isize::MAX;
+    let mut best_at_point = HashMap::new();
+    for pt in all_ts.iter() {
+        let this_str = replace_t_to_s(&adj_str, pt.get()[0], pt.get()[1]);
+        let (this_score, _best_at_point) = find_solution(this_str, best_score, best_at_point);
+        best_at_point = _best_at_point;
+        best_score = min(best_score, this_score)
     }
     best_score.to_string()
+}
+
+fn run_part3_fast(input_str: Vec<String>) -> String {
+    // Swap around S and E and keep going until you find an E (originally S) due to symmetry of move
+    let input_str = input_str
+        .iter()
+        .map(|x| {
+            x.chars()
+                .map(|y| match y {
+                    'S' => 'E',
+                    'E' => 'S',
+                    _ => y,
+                })
+                .join("")
+        })
+        .collect_vec();
+
+    find_solution(input_str, isize::MAX, HashMap::new())
+        .0
+        .to_string()
 }
 
 fn main() {
@@ -134,6 +159,8 @@ fn main() {
     println!("Part 3");
     let input_str = util::read_input(-3);
     println!("Example: {}", run_part3(input_str));
+    let input_str = util::read_input(-3);
+    println!("Example (fast): {}", run_part3_fast(input_str));
     let input_str = util::read_input(3);
-    println!("Actual: {}\n", run_part3(input_str));
+    println!("Actual: {}\n", run_part3_fast(input_str));
 }
