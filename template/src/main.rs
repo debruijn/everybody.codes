0463698a46bf650587bce0a13bedd5ca8d764041
Bert de Bruijn <lpdebruijn@gmail.com>
2024-11-22 16:14:14 -0400

    Include timed run setup to template as well for future days

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,46 +1,36 @@
 use everybody_codes_util as util;
 
-fn run_part1(input_str: Vec<String>) -> String {
+fn run_part1(input_str: Vec<String>, _example: bool) -> String {
     let res = input_str;
-    res.join("")
+    res.join("") + "TODO!"
 }
 
-fn run_part2(input_str: Vec<String>) -> String {
+fn run_part2(input_str: Vec<String>, _example: bool) -> String {
     let res = input_str;
-    res.join("")
+    res.join("") + "TODO!"
 }
 
-fn run_part3(input_str: Vec<String>) -> String {
+fn run_part3(input_str: Vec<String>, _example: bool) -> String {
     let res = input_str;
-    res.join("")
+    res.join("") + "TODO!"
 }
 
 fn main() {
-    // Plan:
-    // - Automatically iterate over all three parts
-    // - Adjust to make it clearer that negative int is example
-    // - Create nice output print function, printing part and whether it is example or actual
-    // - Create template for a joint function that solves all 3
-
     // Part 1: example and actual
     println!("Part 1");
-    let input_str = util::read_input(-1);
-    println!("Example: {}", run_part1(input_str));
-    let input_str = util::read_input(1);
-    println!("Actual: {}\n", run_part1(input_str));
+    util::run(&run_part1, -1);  // Included because first call is slower for allocation reasons
+    util::run(&run_part1, 1);
+    println!("Example: {}", util::run(&run_part1, -1));
+    println!("Actual: {}\n", util::run(&run_part1, 1));
 
-    // Part 2: actual
+    // Part 2: example and actual
     println!("Part 2");
-    let input_str = util::read_input(-2);
-    println!("Example: {}", run_part2(input_str));
-    let input_str = util::read_input(2);
-    println!("Actual: {}\n", run_part2(input_str));
+    println!("Example: {}", util::run(&run_part2, -2));
+    println!("Actual: {}\n", util::run(&run_part2, 2));
 
     // Part 3: example and actual
     println!("Part 3");
-    let input_str = util::read_input(-3);
-    println!("Example: {}", run_part3(input_str));
-    let input_str = util::read_input(3);
-    println!("Actual: {}\n", run_part3(input_str));
+    println!("Example 1: {}", util::run(&run_part3, -3));
+    println!("Actual: {}\n", util::run(&run_part3, 3));
 
 }
